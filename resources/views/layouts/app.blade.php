3c52ea52e69bb76cd24a1379a051ef8279f3c3b4
AfiqAmmar <afiqammar0000@gmail.com>
2022-05-20 21:36:07 +0800

    layout design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,86 +3,237 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Red Card Cafe CMS</title>
 
-    <!-- Scripts -->
+    <!-- Styles -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css?v=3.2.0') }}">
-
-    <!-- Styles -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+    @yield('css')
     
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
 
-                    </ul>
+<body class="hold-transition sidebar-mini pace-red">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-dark" style="background-color: #E40100">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item mr-3">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block mx-2">
+                    <a href="{{ url('support') }}" class="nav-link">Support</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block mx-2">
+                    <a href="{{ url('about') }}" class="nav-link">About</a>
+                </li>
+            </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->fname }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <a href="#" class="brand-link">
+                <img src="img/red_card_cafe_logo-removebg.png" alt="Restaurant Logo" class="brand-image img-circle">
+                <span class="brand-text font-weight-light">Red Card Cafe</span>
+            </a>
+            <div class="sidebar">
+                <!-- Sidebar Menu -->
+                <div class="form-inline">
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column">
+                            @role('master-admin')
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                    <a href="{{ url('dashboard') }}" class="nav-link {{ request()->is('dashboard') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-chart-line"></i>
+                                        <p>
+                                            Dashboard
+                                        </p>
                                     </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('order-queue') }}" class="nav-link {{ request()->is('order-queue') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-list-ul"></i>
+                                        <p>
+                                            Order Queue
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('order-history') }}" class="nav-link {{ request()->is('order-history') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-history"></i>
+                                        <p>
+                                            Order History
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('billing') }}" class="nav-link {{ request()->is('billing') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-credit-card"></i>
+                                        <p>
+                                            Billing
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('staff') }}" class="nav-link {{ request()->is('staff') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-users"></i>
+                                        <p>
+                                            Staff
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('menu') }}" class="nav-link {{ request()->is('menu') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-utensils"></i>
+                                        <p>
+                                            Menu
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('menu-ranking') }}" class="nav-link {{ request()->is('menu-ranking') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-chart-area"></i>
+                                        <p>
+                                            Menu Ranking
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('table') }}" class="nav-link {{ request()->is('table') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-table"></i>
+                                        <p>
+                                            Table
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('tax') }}" class="nav-link {{ request()->is('tax') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-percent"></i>
+                                        <p>
+                                            Tax
+                                        </p>
+                                    </a>
+                                </li>
+                            @endrole
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                            @role('waiter')
+                                <li class="nav-item">
+                                    <a href="{{ url('order-queue') }}" class="nav-link {{ request()->is('order-queue') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-list-ul"></i>
+                                        <p>
+                                            Order Queue
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('order-history') }}" class="nav-link {{ request()->is('order-history') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-history"></i>
+                                        <p>
+                                            Order History
+                                        </p>
+                                    </a>
+                                </li>
+                            @endrole
+
+                            @role('cashier')
+                                <li class="nav-item">
+                                    <a href="{{ url('order-queue') }}" class="nav-link {{ request()->is('order-queue') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-list-ul"></i>
+                                        <p>
+                                            Order Queue
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('order-history') }}" class="nav-link {{ request()->is('order-history') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-history"></i>
+                                        <p>
+                                            Order History
+                                        </p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{ url('billing') }}" class="nav-link {{ request()->is('billing') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-credit-card"></i>
+                                        <p>
+                                            Billing
+                                        </p>
+                                    </a>
+                                </li>
+                            @endrole
+
+                            @role('kitchen-staff')
+                                <li class="nav-item">
+                                    <a href="{{ url('order-queue') }}" class="nav-link {{ request()->is('order-queue') ? 'active' : ''}}">
+                                        <i class="nav-icon fas fa-list-ul"></i>
+                                        <p>
+                                            Order Queue
+                                        </p>
+                                    </a>
+                                </li>
+                            @endrole
+                        </ul>
+                    </nav>
                 </div>
             </div>
-        </nav>
+            <!-- /.sidebar -->
+        </aside>
 
-        <main class="py-4">
+        <!-- Content Wrapper. Contains page content -->
+        <main class="content-wrapper">
             @yield('content')
         </main>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+                Version 1.0.0
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2022 <a target="_blank">Red Card Cafe</a>.</strong> All rights reserved.
+        </footer>
     </div>
+    <!-- ./wrapper -->
+   
 </body>
+<!-- REQUIRED SCRIPTS -->
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('adminlte/dist/js/adminlte.min.js?v=3.2.0') }}"></script>
-    <script src="{{ asset('adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js?v=3.2.0') }}"></script>
+<script src="{{ asset('adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+
 </html>
