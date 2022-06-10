c177453c1a258cfc140e2252b806a004c50bd006
AfiqAmmar <afiqammar0000@gmail.com>
2022-06-10 19:55:36 +0800

    error handling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,7 +57,7 @@
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
@@ -70,7 +70,7 @@
                         @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <input type="password" class="form-control" placeholder="Password" name="password">
+                        <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-lock"></span>
