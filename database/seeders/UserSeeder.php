26c0942ce3d08fad368c2e265d557add328c6593
AfiqAmmar <afiqammar0000@gmail.com>
2022-06-15 16:09:53 +0800

    user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'fname' => 'Master',
+            'lname' => 'Admin',
+            'address' => 'Sample Address',
+            'phone_number' => '0123456789',
+            'gender' => 'Male',
+            'salary' => '1234',
+            'email' => 'masteradmin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'Master-Admin'
+        ]);
+
+        $user->assignRole('master-admin');
+    }
+}
