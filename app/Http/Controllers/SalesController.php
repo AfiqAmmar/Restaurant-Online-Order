6630927c4f520f5ef8662bf9e2f7b3e7cf7d7a6f
AfiqAmmar <afiqammar0000@gmail.com>
2022-08-24 00:48:36 +0800

    daily sales

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -17,23 +17,41 @@ class SalesController extends Controller
     public function index()
     {
         // $today_date = date('Y-m-d');
-        $today_date = date('Y-m-d', strtotime('2022-08-18'));
+        $today_date = date('Y-m-d', strtotime('2022-08-23'));
         $today_arr = array();
+        $categories_arr = array();
+        $today_data_arr = array();
+        $total_today = 0;
         $categories = Category::all();
-        $count = 0;
+
         foreach($categories as $category)
         {
             $today_arr += [$category->name => 0];
         }
-        // $today_arr['Appetizers'] = 10 + $today_arr['Appetizers'];
-        dd($today_arr);
-        $order = Order::whereDate('created_at', $today_date)->get();
-        $menu = $order[3]->menus[0]->id;
-        $category = Menu::find($menu);
-        
-        dd($category->categories->id);
+
+        $orders = Order::whereDate('created_at', $today_date)->get();
+
+        foreach($orders as $order)
+        {
+            foreach($order->menus as $menu)
+            {
+                $total = $menu->price * $menu->pivot->quantity;
+                $menu_category = Menu::find($menu->id);
+                $total_today = $total_today + $total;
+                $today_arr[$menu_category->categories->name] = $total + $today_arr[$menu_category->categories->name];
+            }
+        }
+
+        foreach($categories as $category)
+        {
+            array_push($categories_arr, $category->name);
+            array_push($today_data_arr, $today_arr[$category->name]);
+        }
+
         return view('manage.dashboard.index', [
-            'test' => $test,
+            'categories_arr' => $categories_arr,
+            'today_data_arr' => $today_data_arr,
+            'total_today' => $total_today,
         ]);
     }
 }
--- resources/views/manage/dashboard/index.blade.php
+++ resources/views/manage/dashboard/index.blade.php
@@ -4,10 +4,10 @@
     <section class="content mt-4">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-6">
                     <div class="card card-danger">
                         <div class="card-header">
-                            <h3 class="card-title">Donut Chart</h3>
+                            <h3 class="card-title">Today's Sales</h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
                                     <i class="fas fa-minus"></i>
@@ -18,13 +18,38 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <canvas id="donutChart"
+                            <canvas id="donutChartDaily"
                                 style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;">
                             </canvas>
                         </div>
                         <div class="card-footer bg-default">
                             <div class="text-center">
-                                Total Sales: RM20000
+                                <strong>Total Sales = RM{{ $total_today }}</strong> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card card-danger">
+                        <div class="card-header">
+                            <h3 class="card-title">Weekly Sales</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="donutChartWeekly"
+                                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;">
+                            </canvas>
+                        </div>
+                        <div class="card-footer bg-default">
+                            <div class="text-center">
+                                <strong>Total Sales = RM{{ $total_today }}</strong> 
                             </div>
                         </div>
                     </div>
@@ -37,38 +62,57 @@
 @push('script')
 
     <script src="{{ asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>
+
     <script>
-        let test = <?php echo json_encode($test); ?>;
-        console.log(test);
-        // Get context with jQuery - using jQuery's .get() method.
-        var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+        let categories = <?php echo json_encode($categories_arr); ?>;
+        let datas = <?php echo json_encode($today_data_arr); ?>;
+        var donutChartCanvas = $('#donutChartDaily').get(0).getContext('2d')
         var donutData = {
-        labels: [
-            'Chrome',
-            'IE',
-            'FireFox',
-            'Safari',
-            'Opera',
-            'Navigator',
-        ],
+        labels: categories,
         datasets: [
             {
-            data: [700,500,400,600,300,100],
-            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+            data: datas,
+            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#872657', '#7e587e', '#87afc7', '#00ced1', '#fff380', '#704214', '#f8b88b'], // 13 total colours
             weight: 10,
             }
         ],
         }
-        var donutOptions     = {
+        var donutOptions = {
         maintainAspectRatio : false,
         responsive : true,
         }
-        //Create pie or douhnut chart
-        // You can switch between pie and douhnut using the method below.
+
         new Chart(donutChartCanvas, {
         type: 'doughnut',
         data: donutData,
         options: donutOptions
         })
     </script>
+
+    <script>
+        // let categories = <?php echo json_encode($categories_arr); ?>;
+        // let datas = <?php echo json_encode($today_data_arr); ?>;
+        var donutChartCanvas = $('#donutChartWeekly').get(0).getContext('2d')
+        var donutData = {
+        labels: categories,
+        datasets: [
+            {
+            data: datas,
+            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#872657', '#7e587e', '#87afc7', '#00ced1', '#fff380', '#704214', '#f8b88b'], // 13 total colours
+            weight: 10,
+            }
+        ],
+        }
+        var donutOptions = {
+        maintainAspectRatio : false,
+        responsive : true,
+        }
+
+        new Chart(donutChartCanvas, {
+        type: 'doughnut',
+        data: donutData,
+        options: donutOptions
+        })
+    </script>
+
 @endpush
