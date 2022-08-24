524d7ee6704d5b69aef6f93a7c799f5601fc7474
AfiqAmmar <afiqammar0000@gmail.com>
2022-08-24 21:00:26 +0800

    sales module complete

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Menu;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class SalesController extends Controller
 {
@@ -16,21 +17,33 @@ class SalesController extends Controller
 
     public function index()
     {
-        // $today_date = date('Y-m-d');
-        $today_date = date('Y-m-d', strtotime('2022-08-23'));
+        $today_date = date('Y-m-d');
+        // $today_date = date('Y-m-d', strtotime('2022-08-23'));
         $today_arr = array();
+        $week_arr = array();
+        $month_arr = array();
+        $year_arr = array();
         $categories_arr = array();
         $today_data_arr = array();
+        $week_data_arr = array();
+        $month_data_arr = array();
+        $year_data_arr = array();
         $total_today = 0;
+        $total_week = 0;
+        $total_month = 0;
+        $total_year = 0;
         $categories = Category::all();
 
         foreach($categories as $category)
         {
             $today_arr += [$category->name => 0];
+            $week_arr += [$category->name => 0];
+            $month_arr += [$category->name => 0];
+            $year_arr += [$category->name => 0];
         }
 
+        // daily sales
         $orders = Order::whereDate('created_at', $today_date)->get();
-
         foreach($orders as $order)
         {
             foreach($order->menus as $menu)
@@ -42,16 +55,64 @@ class SalesController extends Controller
             }
         }
 
+        // weekly sales
+        $orders_weeks = Order::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->get();
+        foreach($orders_weeks as $order_week)
+        {
+            foreach($order_week->menus as $menu)
+            {
+                $total = $menu->price * $menu->pivot->quantity;
+                $menu_category = Menu::find($menu->id);
+                $total_week = $total_week + $total;
+                $week_arr[$menu_category->categories->name] = $total + $week_arr[$menu_category->categories->name];
+            }
+        }
+
+        // monthly sales
+        $orders_months = Order::whereMonth('created_at', Carbon::now()->month)->get();
+        foreach($orders_months as $order_month)
+        {
+            foreach($order_month->menus as $menu)
+            {
+                $total = $menu->price * $menu->pivot->quantity;
+                $menu_category = Menu::find($menu->id);
+                $total_month = $total_month + $total;
+                $month_arr[$menu_category->categories->name] = $total + $month_arr[$menu_category->categories->name];
+            }
+        }
+
+        // yearly sales
+        $orders_years = Order::whereYear('created_at', Carbon::now()->year)->get();
+        foreach($orders_years as $order_year)
+        {
+            foreach($order_year->menus as $menu)
+            {
+                $total = $menu->price * $menu->pivot->quantity;
+                $menu_category = Menu::find($menu->id);
+                $total_year = $total_year + $total;
+                $year_arr[$menu_category->categories->name] = $total + $year_arr[$menu_category->categories->name];
+            }
+        }
+
         foreach($categories as $category)
         {
             array_push($categories_arr, $category->name);
             array_push($today_data_arr, $today_arr[$category->name]);
+            array_push($week_data_arr, $week_arr[$category->name]);
+            array_push($month_data_arr, $month_arr[$category->name]);
+            array_push($year_data_arr, $year_arr[$category->name]);
         }
 
         return view('manage.dashboard.index', [
             'categories_arr' => $categories_arr,
             'today_data_arr' => $today_data_arr,
             'total_today' => $total_today,
+            'week_data_arr' => $week_data_arr,
+            'total_week' => $total_week,
+            'month_data_arr' => $month_data_arr,
+            'total_month' => $total_month,
+            'year_data_arr' => $year_data_arr,
+            'total_year' => $total_year,
         ]);
     }
 }
--- resources/views/manage/dashboard/index.blade.php
+++ resources/views/manage/dashboard/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-6">
-                    <div class="card card-danger">
+                    <div class="card card-dark">
                         <div class="card-header">
                             <h3 class="card-title">Today's Sales</h3>
                             <div class="card-tools">
@@ -30,7 +30,7 @@
                     </div>
                 </div>
                 <div class="col-6">
-                    <div class="card card-danger">
+                    <div class="card card-dark">
                         <div class="card-header">
                             <h3 class="card-title">Weekly Sales</h3>
                             <div class="card-tools">
@@ -49,7 +49,59 @@
                         </div>
                         <div class="card-footer bg-default">
                             <div class="text-center">
-                                <strong>Total Sales = RM{{ $total_today }}</strong> 
+                                <strong>Total Sales = RM{{ $total_week }}</strong> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="card card-dark">
+                        <div class="card-header">
+                            <h3 class="card-title">Monthly Sales</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="donutChartMonthly"
+                                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;">
+                            </canvas>
+                        </div>
+                        <div class="card-footer bg-default">
+                            <div class="text-center">
+                                <strong>Total Sales = RM{{ $total_month }}</strong> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card card-dark">
+                        <div class="card-header">
+                            <h3 class="card-title">Yearly Sales</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                                <button type="button" class="btn btn-tool" data-card-widget="remove">
+                                    <i class="fas fa-times"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="donutChartYearly"
+                                style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;">
+                            </canvas>
+                        </div>
+                        <div class="card-footer bg-default">
+                            <div class="text-center">
+                                <strong>Total Sales = RM{{ $total_year }}</strong> 
                             </div>
                         </div>
                     </div>
@@ -90,14 +142,63 @@
     </script>
 
     <script>
-        // let categories = <?php echo json_encode($categories_arr); ?>;
-        // let datas = <?php echo json_encode($today_data_arr); ?>;
+        let datas_week = <?php echo json_encode($week_data_arr); ?>;
         var donutChartCanvas = $('#donutChartWeekly').get(0).getContext('2d')
         var donutData = {
         labels: categories,
         datasets: [
             {
-            data: datas,
+            data: datas_week,
+            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#872657', '#7e587e', '#87afc7', '#00ced1', '#fff380', '#704214', '#f8b88b'], // 13 total colours
+            weight: 10,
+            }
+        ],
+        }
+        var donutOptions = {
+        maintainAspectRatio : false,
+        responsive : true,
+        }
+
+        new Chart(donutChartCanvas, {
+        type: 'doughnut',
+        data: donutData,
+        options: donutOptions
+        })
+    </script>
+
+    <script>
+        let datas_month = <?php echo json_encode($month_data_arr); ?>;
+        var donutChartCanvas = $('#donutChartMonthly').get(0).getContext('2d')
+        var donutData = {
+        labels: categories,
+        datasets: [
+            {
+            data: datas_month,
+            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#872657', '#7e587e', '#87afc7', '#00ced1', '#fff380', '#704214', '#f8b88b'], // 13 total colours
+            weight: 10,
+            }
+        ],
+        }
+        var donutOptions = {
+        maintainAspectRatio : false,
+        responsive : true,
+        }
+
+        new Chart(donutChartCanvas, {
+        type: 'doughnut',
+        data: donutData,
+        options: donutOptions
+        })
+    </script>
+
+    <script>
+        let datas_year = <?php echo json_encode($year_data_arr); ?>;
+        var donutChartCanvas = $('#donutChartYearly').get(0).getContext('2d')
+        var donutData = {
+        labels: categories,
+        datasets: [
+            {
+            data: datas_year,
             backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#872657', '#7e587e', '#87afc7', '#00ced1', '#fff380', '#704214', '#f8b88b'], // 13 total colours
             weight: 10,
             }
