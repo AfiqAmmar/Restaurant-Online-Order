a7a9ba8c1193d5066ebf551225437c566e2d0f29
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2022-09-14 18:36:51 +0800

    UI improvements

--- resources/views/ordering/menus.blade.php
+++ resources/views/ordering/menus.blade.php
@@ -11,14 +11,14 @@
     </nav>
 
     <!-- Categories -->
-    <div class="container bg-danger py-2 d-flex flex-nowrap gap-2 overflow-auto">
-      <a class="btn btn-dark border border-white border-2" href="#predictedMenus" role="button">PREDICTED</a>
+    <div class="container bg-danger py-2 d-flex gap-2 overflow-auto">
+      <a class="btn btn-dark border border-white border-2" href="#recommendedMenus" role="button">RECOMMENDED</a>
       <a class="btn btn-dark border border-white border-2" href="#trendingMenus" role="button">TRENDING</a>
 
       @unless (count($categories)==0)
 
       @foreach ($categories as $category)
-      <a class="btn btn-dark border border-white border-2" href="#{{strtolower($category->name)}}"
+      <a class="btn btn-dark border border-white border-2 text-nowrap" href="#{{strtolower($category->name)}}"
         role="button">{{strtoupper($category->name)}}</a>
       @endforeach
 
@@ -29,7 +29,7 @@
   <main>
     <!-- Menus -->
     <div class="container bg-dark py-2 px-3">
-      <h5 class="text-white py-1" id="predictedMenus">Predicted Menus</h5>
+      <h5 class="text-white py-1" id="recommendedMenus">Recommended Menus</h5>
       <h5 class="text-white py-1" id="trendingMenus">Trending Menus</h5>
 
       @foreach ($categories as $category)
