6001d4f4d16ea20dcab458029d27c39e56c724ee
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2022-09-30 23:04:28 +0800

    Added max and min limit to quantity

--- resources/views/ordering/menu.blade.php
+++ resources/views/ordering/menu.blade.php
@@ -1,6 +1,6 @@
 <x-customer-layout>
   <header class="sticky-top">
-    <!-- Navbar -->
+    {{-- Navbar --}}
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #000">
       <div class="container-fluid">
         <div class="navbar-brand ps-2">
@@ -16,7 +16,7 @@
   <form method="POST" action="/{{$customer_id}}/{{$menu->id}}/cart">
     @csrf
     <main>
-      <!-- Menu -->
+      {{-- Menu --}}
       <div class="container p-3 min-vh-100" style="background-color: #F7E7D8;">
         <img src="{{ asset('menu_img/' . $menu->image_name) }}" class="card-img-top rounded border border-dark"
           alt="{{$menu->name}}">
@@ -38,24 +38,31 @@
         <label for="remarks" class="form-label">
           <h4>Remarks</h4>
         </label>
-        <textarea class="form-control" id="remarks" name="remarks" placeholder="Enter remarks if any..." rows="3"></textarea>
+        <textarea class="form-control" id="remarks" name="remarks" placeholder="Enter remarks if any..."
+          rows="3"></textarea>
       </div>
     </main>
 
-    <!-- Footer -->
+    {{-- Footer --}}
     <footer class="footer px-1 py-3 sticky-bottom" style="background-color: #000">
       <div class="container d-flex justify-content-between align-items-center">
         {{-- Quantity --}}
         <div class="btn-group" role="group" aria-label="Quantity" x-data="{ quantity: 1 }">
-          <button type="button" class="btn btn-light" x-on:click="quantity = quantity-1">
+          <button type="button" class="btn btn-light" x-on:click="quantity = quantity-1"
+            :disabled="(quantity == 1) ? true : false">
             <i class="fas fa-minus"></i>
           </button>
+
           <input type="text" id="quantity" name="quantity" class="btn btn-light disabled" x-model="quantity"
             style="width: 3rem" />
-          <button type="button" class="btn btn-light" x-on:click="quantity = quantity+1">
+
+          <button type="button" class="btn btn-light" x-on:click="quantity = quantity+1"
+            :disabled="(quantity < {{$menu->available_quantity}}) ? false : true">
             <i class="fas fa-plus"></i>
           </button>
         </div>
+
+        {{-- Add menu to cart --}}
         <button type="submit" class="btn btn-success">
           Add&nbsp;&nbsp;<i class="fas fa-cart-plus mr-2"></i>
         </button>
