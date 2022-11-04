a0eba0a1f875efc604908f7805ffea5925599fd9
AfiqAmmar <afiqammar0000@gmail.com>
2022-11-04 09:54:54 +0800

    new seeder

---
+++ database/seeders/TableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Table;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i < 9; $i++)
+        {
+            $table = Table::create([
+                'table_number' => $i,
+                'seats' => 6,
+            ]);
+        }
+    }
+}
