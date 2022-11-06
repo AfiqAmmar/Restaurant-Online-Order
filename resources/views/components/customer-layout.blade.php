3e6797067637aaed924fd410179af9971df295b7
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2022-11-06 12:33:11 +0800

    scroll to category properly when selected

--- resources/views/components/customer-layout.blade.php
+++ resources/views/components/customer-layout.blade.php
@@ -12,6 +12,24 @@
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
   <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <style>
+    @media (prefers-reduced-motion: no-preference) {
+      html:focus-within {
+        scroll-behavior: smooth;
+      }
+
+      @media only screen and (max-width: 768px) {
+        html:focus-within {
+          scroll-padding-top: 8rem;
+        }
+      }
+
+      @media only screen and (min-width: 768px) {
+        html:focus-within {
+          scroll-padding-top: 5rem;
+        }
+      }
+    }
+
     [x-cloak] {
       display: none !important;
     }
@@ -46,6 +64,10 @@
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+  </script>
+  <script>
+    const categories = document.querySelectorAll('h5');
+    categories.forEach(h => h.setAttribute('tabindex', '-1'));
   </script>
 </body>
 
